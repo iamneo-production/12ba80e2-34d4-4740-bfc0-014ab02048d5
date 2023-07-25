fa8aeb464e7cadf3e80f30bb418ea678f527ceed
Maha15092001 <mahalakshmiumadevi@gmail.com>
2023-07-25 07:55:30 +0000

    Final commit

--- dotnetapp/WebApp/Models/checkAlreadyEnrolledCourse.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace WebApp.Models
-{
-    public class checkAlreadyEnrolledCourse{
-        public int userID{get; set;}
-        public int courseID{get; set;}
-    }
-}
