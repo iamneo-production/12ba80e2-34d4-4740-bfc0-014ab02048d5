3fe98d113fe773839efd0121d910dccd11628295
GitGrv <grv.majila07@gmail.com>
2023-07-28 06:13:57 +0000

    Code improved

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -65,6 +66,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgxUiLoaderHttpModule.forRoot({
       showForeground:true,
     }),
+    BrowserAnimationsModule,
     ToastrModule.forRoot(),
     ToastNoAnimationModule.forRoot(),
     NgbModule,
